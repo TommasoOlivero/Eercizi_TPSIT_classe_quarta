e5aa58d34e3d4c90bf1fcf4f215f87b45e0f62ad
Tommaso Olivero <91011609+TommasoOlivero@users.noreply.github.com>
2022-03-03 16:35:02 +0100

    Update main.c

--- Allocazione/main.c
+++ Allocazione/main.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 
 int main() {
-    char *car;
+    char *car;                              //dichiarazione varibili puntatore
     int *num;
 
-    num = (int *) malloc (sizeof(int));
+    num = (int *) malloc (sizeof(int));     //allocazione delle variabili
     car = (char *) malloc (sizeof(char));
 
     *num = 5;
-    *car = 'T';
+    *car = 'T';                             //assegnazione di valori alle vaiarbili
 
-    printf("%d - %d\n", num, *num);
+    printf("%d - %d\n", num, *num);         //stampa delle variabili e del loro indirizzo
     printf("%d - %c", car, *car);
 
     printf("\n");
